cc8b5e39c42ac7e153a13e27943b873a01fb1bac
kasutu <jeromecabugwason2@protonmail.com>
2022-10-09 17:09:49 +0800

    added express with TS

--- init.bat
+++ init.bat
@@ -8,13 +8,37 @@ echo -----------------------------------------------------------------
 echo.
 
 :init
-IF "%~1"=="" GOTO endinit
-IF "%~1"=="ts" npm init -y && npx tsc --init
+IF "%~1"=="ts" GOTO tsinit
+IF "%~1"=="ts-express" GOTO expinit
 SHIFT
+GOTO endinit
 GOTO init
+EXIT /B 0
+
+:tsinit
+CALL npm init -y
+CALL npm i -D typescript ts-node @types/node
+CALL tsc --init
+echo [INFO] ts project created!
+EXIT /B 0
+
+:expinit
+CALL npm init -y 
+CALL npm pkg set scripts.dev="nodemon src/app.ts"
+CALL npm i express -y
+CALL npm i -D typescript ts-node @types/node @types/express nodemon
+CALL tsc --init --outDir ./dist
+CALL mkdir src dist
+echo. 
+echo [INFO] ts-express project created!
+echo [INFO] type "npm run dev" to get started!
+EXIT /B 0
 
 :endinit
 echo [WARN] Please enter a Parameter!
 echo.
-echo            Available Parameters
-echo ts   Initialize a typscript project with node in the current path.
+echo Available Parameters
+echo.
+echo ts               Initialize a typscript project with node in the current path.
+echo ts-express       Initialize a typscript-express project with node in the current path.
+EXIT /B 0
